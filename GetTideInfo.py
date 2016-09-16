e0de5caf0ad852df50dcc9f90599c8391241195d
Neil <Mac@Macs-MacBook-Air.local>
2016-09-15 21:19:29 -0400

    updated README and script

--- GetTideInfo.py
+++ GetTideInfo.py
@@ -1,19 +1,38 @@
+#!/usr/bin/env python
+import sys
 from lxml import html
 import requests
 from datetime import date
+from twython import Twython
 
+#***********
+#Twitter Authentication from apps.twitter.com
+#NOTE: For script to work you'll need to enter in your Twitter Account details below.
+apiKey = ''
+apiSecret = ''
+accessToken = ''
+accessTokenSecret = ''
+#*********
+
+#Get data from NOAA.gov using lxml
 page = requests.get('http://tidesandcurrents.noaa.gov/stationhome.html?id=8534770')
 tree = html.fromstring(page.content)
+tide_info = tree.xpath('//div[@class="span4 well"]/table[@class="table table-condensed"]/tr/td/text() | //div[@class="span4 well"]/table[@class="table table-condensed"]/tr/td/b/text()')
 
-tides = tree.xpath('//div[@class="span4 well"]/table[@class="table table-condensed"]/tr/td/text()')
-high_tide = tree.xpath('//div[@class="span4 well"]/table[@class="table table-condensed"]/tr/td/b/text()')
+#Clean up tide_info. NOAA site can place random " ft." strings in list. 
+tide_info.remove(" ft.")
+tide_info = [item.split(' ft.')[0] for item in tide_info]
 
+#Prepare tweet message
 def prepare_twitter_message():
 	today_str = "Tide Information for {:%b %d, %Y}: \n".format(date.today())
-	low_tide_am = "Low: {} - {}\n".format(tides[0], tides[2])
-	high_tide_am = "High: {} - {}\n".format(tides[3], tides[5])
-	low_tide_pm = "Low: {} - {}\n".format(tides[6], tides[8])
-	high_tide_pm = "High: {} - {} ft.".format(high_tide[0], high_tide[2])
-	print(today_str + low_tide_am + high_tide_am + low_tide_pm + high_tide_pm)
+	low_tide_am = "Low: {}  {} ft\n".format(tide_info[0], tide_info[2])
+	high_tide_am = "High: {}  {} ft\n".format(tide_info[3], tide_info[5])
+	low_tide_pm = "Low: {}  {} ft\n".format(tide_info[6], tide_info[8])
+	high_tide_pm = "High: {}  {} ft".format(tide_info[9], tide_info[11])
+	
+	return today_str + low_tide_am + high_tide_am + low_tide_pm + high_tide_pm
 
-prepare_twitter_message()
+#Configure Tweet and send.
+api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
+api.update_status(status=prepare_twitter_message())	
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-work in progress....
+# [@HighTideVentnor](https://twitter.com/HighTideVentnor)
+
+A script that tweets daily the High and Low tides for Ventnor City, NJ. 🌊
